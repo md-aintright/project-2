8389219c99732d8f474875b2e4d1d3e948fea5f8
keggert <64288336+keggert@users.noreply.github.com>
2020-09-16 21:18:07 -0500

    Made some edits

---
+++ table-subplot.js
@@ -0,0 +1,111 @@
+
+  Plotly.d3.csv("Happiness.csv", function(err, rows){
+    function unpack(rows, key) {
+      return rows.map(function(row) { return row[key]; });
+      }
+
+      // header values
+      var headerNames = Plotly.d3.keys(rows[0]);
+      var headerValues = [headerNames[1],headerNames[2],headerNames[3],
+                          headerNames[4], headerNames[5],headerNames[6]];
+    
+      // cell values
+      var cellValues = [];
+      for (i = 0; i < headerValues.length; i++) {
+        cellValue = unpack(rows, headerValues[i]);
+        cellValues[i] = cellValue;
+      }
+      console.log(cellValues);
+
+      // create table
+      var table = {
+        type: 'table',
+        columnwidth: [300,450,350,350,350,350],
+        columnorder: [1,2,3,4,5,6],
+        header: {
+          values: headerValues,
+          align: "center",
+          line: {width: 1, color: 'rgb(35, 75, 50)'},
+          fill: {color: ['rgb(155, 150, 280)']},
+          font: {family: "Arial", size: 12, color: "white"}
+        },
+        cells: {
+          values: cellValues,
+          align: ["center", "center"],
+          line: {color: "black", width: 1},
+          fill: {color: ['rgb(220, 183, 288)', 'rgba(208, 202, 209, 0.65)']},
+          font: {family: "Arial", size: 11, color: ["black"]}
+        },
+        xaxis: 'x',
+        yaxis: 'y',
+        domain: {x: [0,0.4], y: [0,1]}
+      }
+    
+      // create 1st plot
+      var trace1 = {
+        x: unpack(rows, 'Year'),
+        y: unpack(rows, 'EPRank'),
+        xaxis: 'x1',
+        yaxis: 'y1',
+        mode: 'lines',
+        line: {width: 2, color: 'Red'},
+        name: 'Emotional & Physical Well-Being Rank'
+      }
+      // create 2nd plot
+      var trace2 = {
+        x: unpack(rows, 'Year'),
+        y: unpack(rows, 'IERank'),
+        xaxis: 'x2',
+        yaxis: 'y2',
+        mode: 'lines',
+        line: {width: 2, color: 'Green'},
+        name: 'Income & Employment Rank'
+      }
+    
+      // create 3rd plot
+      var trace3 = {
+        x: unpack(rows, 'Year'),
+        y: unpack(rows, 'CERank'),
+        xaxis: 'x3',
+        yaxis: 'y3',
+        mode: 'lines',
+        line: {width: 2, color: 'Blue'},
+        name: 'Community and Environment Rank'
+      }
+    
+      var data = [table,trace1,trace2,trace3]
+    
+      // define subplot axes
+      var axis = {
+        showline: true,
+        zeroline: false,
+        showgrid: true,
+        mirror:true,
+        ticklen: 4,
+        gridcolor: '#ffffff',
+        tickfont: {size: 10},
+      }
+    
+      var axis1 = {domain: [0.5, 1.5], anchor: 'y1', showticklabels: false}
+      var axis2 = {domain: [0.5, 1], anchor: 'y2', showticklabels: false}
+      var axis3 = {domain: [0.5, 1], anchor: 'y3', tickformat: 'd'}
+      var axis4 = {domain: [0.66, 0.98], anchor: 'x1', hoverformat: '.2f'}
+      var axis5 = {domain: [0.34, 0.64], anchor: 'x2', hoverformat: '.2f'}
+      var axis6 = {domain: [0.0, 0.32], anchor: 'x3', hoverformat: '.2f'}
+    
+      // define layout
+      var layout = {
+        title: "The Happiness Index for Top 5 Cities in the USA from 2017-2020",
+        plot_bgcolor: 'rgba(228, 222, 249, 0.65)',
+        showlegend: false,
+        xaxis1: Object.assign(axis1,axis),
+        xaxis2: Object.assign(axis2,axis),
+        xaxis3: Object.assign(axis3,axis),
+        yaxis1: Object.assign(axis4,axis),
+        yaxis2: Object.assign(axis5,axis),
+        yaxis3: Object.assign(axis6,axis),
+      }
+    
+      Plotly.newPlot('plot', data, layout);
+    
+  });
